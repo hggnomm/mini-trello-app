60deb73e38a99f21c9431bd5a61e3203a2e3bdea
hggnomm <hggnomm.dev@gmail.com>
2026-06-24 14:31:18 +0700

    feature: init socketio

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,18 @@
 import { settings } from './utils/settings';
 import { createApp } from './app';
 import { logger } from './utils/logger';
+import http from 'http';
+import { initSocket } from './socket/socket';
 
 const port = settings.PORT;
 
 const app = createApp();
+const server = http.createServer(app);
+
+initSocket(server);
 
 if (require.main === module) {
-    app.listen(port, () => {
+    server.listen(port, () => {
         logger.info(`App running on port ${port}.`);
     });
 }
---
+++ frontend/src/constants/socket.constant.ts
@@ -0,0 +1,5 @@
+export const SOCKET_EVENTS = {
+  BOARD_JOIN: "board:join",
+  BOARD_LEAVE: "board:leave",
+  CARD_CREATED: "card:created",
+} as const;
